07b90df3ab19a94fe0067977da472cc2175f97f9
vu.truong <vu.truong@saigontechnology.com>
2021-12-31 15:28:55 +0700

    Update Demo Page

---
+++ static/js/313.6ee0b358.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklession_02=self.webpackChunklession_02||[]).push([[313],{313:function(n,s,e){e.r(s),e.d(s,{default:function(){return c}});var a=e(724),r=e(184);function c(n){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)(a.Z,{title:"Dashboard"}),(0,r.jsx)("div",{className:"card-container",children:(0,r.jsx)("div",{className:"card-content",children:"Dashboard"})})]})}}}]);
+//# sourceMappingURL=313.6ee0b358.chunk.js.map
